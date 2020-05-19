2f268dcb848a8ab8173e688a877b51c26f342894
Yassif <yassif.n.m@gmail.com>
2020-05-19 16:02:42 +0800

    created the model.py the database model to store the data that the user will enter into the admin blog app. Then migrated the database to take into account the models. Set the html text to loop through the data and output the blog into the template.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from portfolio import views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
